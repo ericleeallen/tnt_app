6b2b2271037ed2f3e7ca57b0213f37c8281eebcc
Eric Allen <eric.lee.allen@gmail.com>
2013-03-17 20:46:55 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TntApp::Application.config.secret_token = '6ec2330b8a31e1b7f52006749a839fb44596e617e417f2c1a60d0ce153ce2d6e56fefa6a6f8e490db7b5f843f477d8ceea6d726c622ce8b9c05cab1914f2ff53'
